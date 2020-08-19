94e9bbf4abab29a3a8e55b234b79988805a0cd19
19032003 <19032003@DESKTOP-7QSHEOQ>
2020-08-19 12:55:15 +0800

    Updated Account.java

--- C206_CaseStudy/src/Account.java
+++ C206_CaseStudy/src/Account.java
@@ -1,4 +1,57 @@
+import java.util.ArrayList;
 
+//Christine
 public class Account {
+	
+	private String username;
+	private String password;
+	private String userRole;
+	
 
+	//constructor
+	public Account(String username, String password, String userRole) {
+		this.username = username;
+		this.password = password;
+		this.userRole = userRole;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getUserRole() {
+		return userRole;
+	}
+
+
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+	
+	public String toString() {
+		String output = String.format("Username: %-10s", username);
+		output += String.format("Password: ", password);
+		output += String.format("userRole: ", userRole);
+		return output;
+		
+	}
 }
+	
+
