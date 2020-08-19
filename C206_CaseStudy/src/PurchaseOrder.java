25698f8b29ea3798b5fe045b248c71e9bbe2587e
19017515 <19017515@DESKTOP-JJ7N3S4>
2020-08-19 13:28:16 +0800

    Done PurchaseOrder

--- C206_CaseStudy/src/PurchaseOrder.java
+++ C206_CaseStudy/src/PurchaseOrder.java
@@ -1,4 +1,56 @@
 // Samantha
+/**
+ * @author 19017515
+ *
+ */
 public class PurchaseOrder {
+	private String itemName;
+	private int itemQuantity;
+	private double itemUnitPrice;
+	private String deliveryDate;
+	
+	public PurchaseOrder(String itemName, int itemQuantity, double itemUnitPrice, String deliveryDate) {
+		this.itemName = itemName;
+		this.itemQuantity = itemQuantity;
+		this.itemUnitPrice = itemUnitPrice;
+		this.deliveryDate = deliveryDate;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public int getItemQuantity() {
+		return itemQuantity;
+	}
+
+	public void setItemQuantity(int itemQuantity) {
+		this.itemQuantity = itemQuantity;
+	}
+
+	public double getItemUnitPrice() {
+		return itemUnitPrice;
+	}
+
+	public void setItemUnitPrice(double itemUnitPrice) {
+		this.itemUnitPrice = itemUnitPrice;
+	}
+
+	public String getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(String deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+	public String toString() {
+		String output = String.format("%-10s %-10d %.2f %-10s", itemName, itemQuantity, itemUnitPrice, deliveryDate);
+		return output;
+	}
+	
 
 }
