4ea05f577b55cc41f1a4ca2f802c02dd72499481
19005338 <19005338@172.24.23.179>
2020-08-19 21:36:11 +0800

    Create method in CanteenApp

--- C206_CaseStudy/src/CanteenApp.java
+++ C206_CaseStudy/src/CanteenApp.java
@@ -1,8 +1,77 @@
+import java.util.ArrayList;
 
 public class CanteenApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	public void userTypeMenu() {
+		
+	}
+	
+	public void optionMenu() {
+		
+	}
+	
+	public void addMenuItem(ArrayList<MenuItem> menuList) {
+		
+	}
+	
+	public void deleteMenuItem(ArrayList<MenuItem> menuList) {
+		
+	}
+	
+	public void viewAllMenuItem(ArrayList<MenuItem> menuList) {
+		
+	}
+	
+	public void addAccount(ArrayList<Account> accountList) {
+		
+	}
+	
+	public void deleteAccount(ArrayList<Account> accountList) {
+		
+	}
+	
+	public void viewallAccount(ArrayList<Account> accountList) {
+		
+	}
+	
+	public void addOrder(ArrayList<Order> orderList) {
+		
+	}
+	
+	public void deleteOrder(ArrayList<Order> orderList) {
+		
+	}
+	
+	public void viewAllOrder(ArrayList<Order> orderList) {
+		
+	}
+	
+	public void addPurchaseOrder(ArrayList<PurchaseOrder> PurchaseOrderList) {
+		
+	}
+	
+	public void deletePurchaseOrder(ArrayList<PurchaseOrder> PurchaseOrderList) {
+		
+	}
+	
+	public void viewAllPurchaseOrder(ArrayList<PurchaseOrder> PurchaseOrderList) {
+		
+	}
+	
+	public void addPromotion(ArrayList<Promotion> promotionList) {
+		
+	}
+	
+	public void deletePromotion(ArrayList<Promotion> promotionList) {
+		
+	}
+	
+	public void viewAllPromotion(ArrayList<Promotion> promotionList) {
 		
 	}
 
