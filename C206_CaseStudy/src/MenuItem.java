8ed7a9956f6709e248264fed6f73e16edc6052af
19016598 <19016598@DESKTOP-923GIJA>
2020-08-19 12:23:22 +0800

    MenuItem done (shangeetha)

--- C206_CaseStudy/src/MenuItem.java
+++ C206_CaseStudy/src/MenuItem.java
@@ -1,4 +1,40 @@
 
 public class MenuItem {
+	private String category;
+	private String name;
+	private Double price; 
+	
+	public MenuItem(String category, String name, Double price) {
+		this.category = category;
+		this.name = name;
+		this.price = price;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	public String toString() {
+		String output = String.format("%-20s %-20s %-.2f", category,name,price);
+		
+		return output;
+		
+		
+		
+	}
 
 }
